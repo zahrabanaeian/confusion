0c8836e457df9777acfed08c76083c870587a1da
zahrabanaeian <zahrabanaeian@gmail.com>
2020-04-23 15:35:14 +0430

    fisrt

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -35,7 +35,7 @@ const About = ({ leaders, isLoading, errMess }) => {
             of world fusion cuisine that can be found nowhere else, it enjoys
             patronage from the A-list clientele in Hong Kong. Featuring four of
             the best three-star Michelin chefs in the world, you never know what
-            will arrive on your plate the next time you visit us. zahra
+            will arrive on your plate the next time you visit us.
           </p>
           <p>
             The restaurant traces its humble beginnings to{" "}
